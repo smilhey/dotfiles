67f2cc4e88adf59e0d567a08afb790233a2d6a08
Smail El Hafdi <smail.elhafdi@gmail.com>
2024-04-30 12:11:16 +0200

    Unused plugins

--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.keymap.set("n", "<leader>tt", function()
-			require("trouble").toggle("workspace_diagnostics")
-		end, { silent = true, noremap = true })
-	end,
-}
