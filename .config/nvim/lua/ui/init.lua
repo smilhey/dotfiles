a88f8d2398156800e639eff58bc82dda4897f761
Smail El Hafdi <smail.elhafdi@gmail.com>
2024-06-04 13:04:19 +0200

    keep up with the update

--- .config/nvim/lua/ui/init.lua
+++ .config/nvim/lua/ui/init.lua
@@ -1,7 +1,12 @@
 local messages = require("ui.messages")
+local cmdwin = require("ui.cmdwin")
 local cmdline = require("ui.cmdline")
+local popupmenu = require("ui.popupmenu")
 local win_input = require("ui.input")
 local win_select = require("ui.select")
+local scrollbar = require("ui.scrollbar")
+local notif = require("ui.notif")
+local lsp_progress =require("ui.lsp_progress")
 
 vim.ui.input = function(opts, on_confirm)
 	win_input(opts, on_confirm, { relative = "cursor", row = 1, col = 1 })
@@ -11,5 +16,37 @@ vim.ui.select = function(items, opts, on_choice)
 	win_select(items, opts, on_choice, {})
 end
 
-messages.init()
-cmdline.init()
+messages.setup()
+popupmenu.setup({})
+cmdwin.setup()
+scrollbar.setup()
+notif.setup()
+lsp_progress.setup()
+-- cmdline.setup()
+
+local function ui_cmd(args)
+	local ui_element = args.fargs[1]
+	if ui_element == "cmdline" then
+		cmdwin.toggle()
+	elseif ui_element == "popupmenu" then
+		popupmenu.toggle()
+	elseif ui_element == "messages" then
+		messages.toggle()
+	elseif ui_element == "scrollbar" then
+		scrollbar.toggle()
+	elseif ui_element == "notif" then
+		notif.toggle()
+	else
+		vim.notify("No function arguments provided", vim.log.levels.WARN)
+	end
+end
+
+local function ui_cmd_complete(ArgLead, _)
+	local items = { "cmdline", "popupmenu", "messages", "scrollbar", "notif" }
+	local completion_list = vim.tbl_filter(function(v)
+		return string.find(v, "^" .. ArgLead) ~= nil
+	end, items)
+	return completion_list
+end
+
+vim.api.nvim_create_user_command("Ui", ui_cmd, { desc = "ui command", nargs = "?", complete = ui_cmd_complete })
