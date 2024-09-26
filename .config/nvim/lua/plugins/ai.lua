6f0ec6b39245caf19f3b617edabe8a7d943eb72b
Smail El Hafdi <smail.elhafdi@gmail.com>
2024-09-26 10:13:39 +0200

    ai modif

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,81 @@
+return {
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+		dependencies = {
+			"zbirenbaum/copilot.lua",
+			cmd = "Copilot",
+			config = function()
+				require("copilot").setup({
+					-- suggestion = { enabled = true, keymap = { accept = "<M-;>" } },
+					suggestion = { enabled = false },
+					panel = { enabled = false },
+					filetypes = {
+						markdown = true,
+					},
+				})
+			end,
+		},
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "copilot",
+						keymaps = {
+							close = {
+								modes = {
+									n = "q",
+									i = "<C-q>",
+								},
+								index = 3,
+								callback = "keymaps.close",
+								description = "Close Chat",
+							},
+							stop = {
+								modes = {
+									n = "C-q",
+								},
+								index = 4,
+								callback = "keymaps.stop",
+								description = "Stop Request",
+							},
+						},
+					},
+					inline = {
+						adapter = "copilot",
+					},
+					agent = {
+						adapter = "copilot",
+					},
+				},
+				display = {
+					action_palette = {
+						width = 95,
+						height = 10,
+					},
+					chat = {
+						window = {
+							layout = "float",
+							relative = "editor",
+							width = 0.45,
+							height = 0.85,
+							row = 1,
+							col = 90,
+							zindex = 50,
+						},
+					},
+				},
+			})
+			vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionToggle<cr>", { noremap = true, silent = true })
+		end,
+	},
+}
